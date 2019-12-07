a8cdfa3e58c400346bf89dfdd6a6f63a3555af33
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-12-07 09:54:13 +0100

    bryce working

---
+++ Fitness party/Assets/Scripts/BryceMoves.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BryceMoves : MonoBehaviour
+{
+    public GameObject bryce;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = bryce.GetComponent<Animator>();
+    }
+
+    public void DoNothing()
+    {
+        animator.SetTrigger("doNothing");
+    }
+
+    public void Walking()
+    {
+        animator.SetTrigger("walk");
+    }
+
+    public void Runnning()
+    {
+        animator.SetTrigger("run");
+    }
+
+    public void Jogging()
+    {
+        animator.SetTrigger("jog");
+    }
+
+    public void JumpingJacks()
+    {
+        animator.SetTrigger("jumpingjacks");
+    }
+
+    public void NarutoRun()
+    {
+        animator.SetTrigger("narutoRun");
+    }
+}
