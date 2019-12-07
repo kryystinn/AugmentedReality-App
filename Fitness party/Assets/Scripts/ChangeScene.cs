70a2db2e5cc9112a52886f5135d7d4f41366ae32
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-12-07 11:52:18 +0100

    jody scene

---
+++ Fitness party/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void BryceScene()
+    {
+        SceneManager.LoadScene("BryceScene");
+    }
+
+    public void JodyScene()
+    {
+        SceneManager.LoadScene("JodyScene");
+    }
+
+    public void JasperScene()
+    {
+        SceneManager.LoadScene("JasperScene");
+    }
+}
---
+++ Fitness party/Assets/Scripts/JodyMoves.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JodyMoves : MonoBehaviour
+{
+    public GameObject jody;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = jody.GetComponent<Animator>();
+    }
+
+    public void DoNothing()
+    {
+        animator.SetTrigger("doNothing");
+    }
+
+    public void LayDown()
+    {
+        animator.SetTrigger("layDown");
+    }
+
+    public void CircleCrunch()
+    {
+        animator.SetTrigger("circleCrunch");
+    }
+
+    public void BicycleCrunch()
+    {
+        animator.SetTrigger("bicycleCrunch");
+    }
+
+    public void WarmUp()
+    {
+        animator.SetTrigger("warmUp");
+    }
+
+    public void Energy()
+    {
+        animator.SetTrigger("energy");
+    }
+}
